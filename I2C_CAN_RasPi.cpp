38f724ff55ad5c743c970ebc63fddf9fccd74193
ekrimsk <ekrimsky@stanford.edu>
2022-06-14 15:01:47 -0700

    dbg

--- I2C_CAN_RasPi.cpp
+++ I2C_CAN_RasPi.cpp
@@ -106,12 +106,15 @@ bool I2C_CAN::IIC_CAN_GetReg(unsigned char __reg, unsigned char *__dta)
 
     // returns data.byte & 0xFF (so only LSB) OR -1 if fails 
     int tmp = wiringPiI2CReadReg8(_fd, __reg);
+
+    printf("get reg 8 %i\n", tmp);
     
     if (tmp == -1) {
         return false;
 
     } else {
-        *__dta = tmp;   
+        *__dta = (uint8_t) tmp;  
+        printf("get reg 8 point %i\n", __dta); 
         return true;
     }
 }
