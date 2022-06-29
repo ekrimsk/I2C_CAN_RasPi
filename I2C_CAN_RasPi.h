bb687b4227231cc1f650660fc9c738f0d1f16e12
ekrimsk <ekrimsky@stanford.edu>
2022-06-28 18:14:22 -0700

    supoer short write delay

--- I2C_CAN_RasPi.h
+++ I2C_CAN_RasPi.h
@@ -10,7 +10,9 @@
 
 
 // 100 works, 50 seems to mostly work but theres not much time to be saved here really 
-#define MIN_WRITE_DELAY 50 
+//#define MIN_WRITE_DELAY 50 
+#define MIN_WRITE_DELAY 10 
+
 
 #include "I2C_CAN_dfs.h"
 
