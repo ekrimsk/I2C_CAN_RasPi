58b0ac505fbc56590abdc9a60a3a31eb74966360
ekrimsk <ekrimsky@stanford.edu>
2022-08-03 16:15:26 -0700

    back to 20 to test

--- I2C_CAN_RasPi.h
+++ I2C_CAN_RasPi.h
@@ -12,7 +12,8 @@
 
 
 // 100 works, 50 seems to mostly work but theres not much time to be saved here really 
-#define MIN_WRITE_DELAY 100 
+//#define MIN_WRITE_DELAY 100 
+#define MIN_WRITE_DELAY 20
 //#define MIN_WRITE_DELAY 10  
 
 
